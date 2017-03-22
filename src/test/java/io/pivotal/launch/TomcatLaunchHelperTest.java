0fa91d5bff57dc130bb42d62ad40cff44b38c9bc
Mark Alston <malston@pivotal.io>
2017-03-21 20:06:20 -0600

    Remove properties

--- src/test/java/io/pivotal/launch/TomcatLaunchHelperTest.java
+++ src/test/java/io/pivotal/launch/TomcatLaunchHelperTest.java
@@ -8,10 +8,30 @@ import java.util.HashMap;
 
 public class TomcatLaunchHelperTest {
 
-    @Test(expected = NullPointerException.class)
-    public void testGetResource() throws Exception {
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetResourceThrowsIllegalArgumentException() throws Exception {
         TomcatLaunchHelper helper = new TomcatLaunchHelper();
         helper.createContainerDataSource(new HashMap<>());
+    }
+
+    @Test
+    public void testGetValidResource() {
+        HashMap<String, Object> credentials = new HashMap<>();
+        credentials.put("name", "jdbc/serviceName");
+        credentials.put("driverClassName", "com.mysql.cj.jdbc.Driver");
+        credentials.put("url", "jdbc:mysql://root:password@localhost/mysql");
+        credentials.put("username", "username");
+        credentials.put("password", "password");
+        credentials.put("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
+
+        TomcatLaunchHelper helper = new TomcatLaunchHelper();
+        helper.createContainerDataSource(credentials);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetEnvironmentThrowsIllegalArgumentException() throws Exception {
+        TomcatLaunchHelper helper = new TomcatLaunchHelper();
+        helper.getEnvironment("test", null);
     }
 
     @Test

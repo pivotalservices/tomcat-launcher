795b79589936d037e724bdebfd45bb085ee83d96
Mark Alston <malston@pivotal.io>
2017-03-24 15:25:37 -0600

    Add test for config precedence

--- src/test/java/io/pivotal/launch/TomcatConfigurerTest.java
+++ src/test/java/io/pivotal/launch/TomcatConfigurerTest.java
@@ -1,18 +1,25 @@
 package io.pivotal.launch;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.descriptor.web.ContextEnvironment;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.PropertySource;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Some of these are integration tests... Need to run config server on port 8888
+ * and point it to github.com/malston/config-repo. TODO: Move integration tests
+ * into separate folder.
+ *
+ * @author malston
+ */
 public class TomcatConfigurerTest {
 
     @Rule
@@ -20,44 +27,74 @@ public class TomcatConfigurerTest {
 
     @Test
     public void testLoadConfiguration() throws Exception {
-        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application", new String[] {"default"});
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application",
+                new String[] { "default" });
+        tomcatConfigurer.setConfigurationLoader(() -> new PropertySource.StubPropertySource("test"));
         PropertySource<?> source = tomcatConfigurer.loadConfiguration();
         assertThat(source, instanceOf(PropertySource.StubPropertySource.class));
+        assertEquals("test", source.getName());
+    }
+
+    @Test
+    public void testConfigPrecedenceOrder() throws Exception {
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "foo",
+                new String[] { "development, db" });
+        CompositePropertySource source = (CompositePropertySource) tomcatConfigurer.loadConfiguration();
+        assertNotNull(source);
+        assertEquals("mycooldb", source.getProperty("foo.db"));
+        assertThat("property sources", source.getPropertySources().size(), equalTo(10));
+        assertThat(source.getPropertySources().stream()
+                        .map(PropertySource::getName)
+                        .collect(toList()),
+                contains("configClient",
+                        "https://github.com/malston/config-repo/foo-db.properties",
+                        "https://github.com/malston/config-repo/foo-development.properties",
+                        "https://github.com/malston/config-repo/foo.properties",
+                        "https://github.com/malston/config-repo/application.yml",
+                        "systemProperties",
+                        "systemEnvironment",
+                        "random",
+                        "applicationConfig: [profile=]",
+                        "defaultProperties"));
     }
 
     @Test
     public void testCreateStandardContext() throws Exception {
-        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application", new String[] {"default"});
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application",
+                new String[] { "default" });
         Tomcat tomcat = new Tomcat();
         StandardContext ctx = tomcatConfigurer.createStandardContext(tomcat);
-        Assert.assertNotNull(ctx);
+        assertNotNull(ctx);
     }
 
     @Test
     public void testLoadLocalConfigurationFromConfigServer() throws Exception {
-        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888","application", new String[] {"default"});
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application",
+                new String[] { "default" });
         PropertySource<?> source = tomcatConfigurer.loadConfiguration();
-        Assert.assertNotNull(source);
+        assertNotNull(source);
         ContextEnvironment ctxEnv = tomcatConfigurer.getEnvironment(source, "foo");
         assertEquals(ctxEnv.getValue(), "baz");
     }
 
     @Test
     public void testLoadLocalConfigurationFromFile() throws Exception {
-        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://bogus", "application", new String[] {"default"});
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://bogus", "application",
+                new String[] { "default" });
         PropertySource<?> source = tomcatConfigurer.loadConfiguration();
-        Assert.assertNotNull(source);
+        assertNotNull(source);
         ContextEnvironment ctxEnv = tomcatConfigurer.getEnvironment(source, "foo");
         assertEquals(ctxEnv.getValue(), "mark_laptop");
     }
 
     @Test
     public void testLoadEnvironmentVariableFromConfigServer() throws Exception {
-        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application", new String[] {"default"});
+        TomcatConfigurer tomcatConfigurer = new TomcatConfigurer("http://localhost:8888", "application",
+                new String[] { "default" });
         environmentVariables.set("CONFIG_TEST", "foobar");
         assertEquals("foobar", System.getenv("CONFIG_TEST"));
         PropertySource<?> source = tomcatConfigurer.loadConfiguration();
-        Assert.assertNotNull(source);
+        assertNotNull(source);
         ContextEnvironment ctxEnv = tomcatConfigurer.getEnvironment(source, "CONFIG_TEST");
         assertEquals(ctxEnv.getValue(), "foobar");
     }
